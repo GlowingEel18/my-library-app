97c23a9643dc74c616a9624dedb1d87c759d1737
Hardik Marlapudi <hardik.a.marlapudi@gmail.com>
2024-11-03 19:19:01 -0500

    ready to deploy

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,12 @@
 // src/App.js
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import About from './pages/About';
-import Catalog from './pages/Catalog';
-import Search from './pages/Search';
-import Contact from './pages/Contact';
-import Footer from './components/Footer';
-import './BookItem.css';
-import './Genre.css';
+import { HashRouter as Router } from 'react-router-dom';
 
-const App = () => (
-  <Router>
-    <Header />
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    <Footer />
-  </Router>
-);
+function App() {
+  return (
+    <Router>
+      {/* Your routes go here */}
+    </Router>
+  );
+}
 
 export default App;
