2fe0992dac0a635ce3e13e39ef060699c723ae77
Hardik Marlapudi <hardik.a.marlapudi@gmail.com>
2024-11-03 19:59:22 -0500

    Fix deployment issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
